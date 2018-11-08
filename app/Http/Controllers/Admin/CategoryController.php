c224c33f31db32ad4a7eabed5809849f7aef01f3
AlexBoxx <home-autoschool@yandex.ru>
2018-11-08 21:42:11 +0400

    Update app/Http/Controllers/Admin/CategoryController.php

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $data['title'] = 'Разделы';
+      $data['categories'] = Category::paginate(10);
+
+       //dd($data['categories']);
+
+        return view('admin.categories.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      $data['title'] = 'Добавить раздел';
+      $data['category'] = [];
+      $data['categories'] = Category::with('children')->where('parent_id', '0')->get();
+      $data['delimiter'] = '';
+        return view('admin.categories.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Category::create($request->all());
+
+        return redirect()->route('admin.category.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+
+      $data['category'] = $category;
+      $data['categories'] = Category::with('children')->where('parent_id', '0')->get();
+      $data['delimiter'] = '';
+      $data['title'] = 'Редактировать - '.$category['title'];
+
+      return view('admin.categories.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->update($request->except('slug'));
+        return redirect()->route('admin.category.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('admin.category.index');
+    }
+}
