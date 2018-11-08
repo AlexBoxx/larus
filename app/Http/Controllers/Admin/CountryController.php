d04a4d7cbf809d0fe059405ce4f44e1c17fbeef8
AlexBoxx <home-autoschool@yandex.ru>
2018-11-08 21:42:15 +0400

    Update app/Http/Controllers/Admin/CountryController.php

---
+++ app/Http/Controllers/Admin/CountryController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Country;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $data['title'] = 'Страны';
+      $data['countres'] = Country::paginate(10);
+
+        return view('admin.country.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      $data['title'] = 'Добавление';
+      $data['country'] = [];
+
+        return view('admin.country.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $validator = $request->validate([
+          'title' => 'required|string|max:255|min:3',
+      ]);
+
+      Country::create([
+        'title' => $request['title'],
+
+      ]);
+
+      return redirect()->route('admin.country.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Country $country)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Country $country)
+    {
+
+      $data['country'] = $country;
+      $data['title'] = 'Редактирование- '.$country['title'];
+
+        return view('admin.country.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Country $country)
+    {
+      $validator = $request->validate([
+          'title' => 'required|string|max:255|min:3',
+      ]);
+
+      $country->title = $request['title'];
+      $country->save();
+
+      return redirect()->route('admin.country.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Country $country)
+    {
+        $country->delete();
+        return redirect()->route('admin.country.index');
+    }
+}
